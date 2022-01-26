8aa68a2db529bbd1898ccdb8ff4a20022e8241d0
starkiller17 <eduardo.said.mndz@gmail.com>
2022-01-26 05:27:39 -0600

    js course add using classes and OOP on an application

---
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,68 @@
+class Tooltip {
+
+}
+
+class ProjectItem {
+  constructor(projectId, updateProjectListFunction) {
+    this.id = projectId;
+    this.updateProjectListHandler = updateProjectListFunction;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton() {}
+
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn.addEventListener('click', this.updateProjectListHandler);
+  }
+
+}
+
+class ProjectList {
+  projects = [];
+
+  constructor(type) {
+    this.type = type;
+
+    const projectItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of projectItems) {
+      this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
+    }
+    console.log(this.projects);
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+  
+  addProject() {
+    console.log(this);
+  }
+
+  switchProject(projectId) {
+    //const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    //this.projects.splice(projectIndex, 1);
+
+    this.switchHandler(this.projects.find(p => p.id === projectId));
+    // Returns all items but the one that is going to be dropped
+    this.projects = this.projects.filter(p => p.id !== projectId);
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectList = new ProjectList('active');
+    const finishedProjectList = new ProjectList('finished');
+
+    activeProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandlerFunction(
+      activeProjectList.addProject.bind(activeProjectList)
+    );
+  }
+}
+
+App.init();
